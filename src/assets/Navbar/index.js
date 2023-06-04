2fca0789ee6760dcc6233edafc9a5c6900d629eb
Uzzwal Maharjan <ujolmaharjan94@gmail.com>
2023-06-04 19:07:19 +0545

    add

--- src/assets/Navbar/index.js
+++ src/assets/Navbar/index.js
@@ -2,6 +2,5 @@ import cart from '../Navbar/cart-outline.svg'
 import logo from '../Navbar/logo-regular.png'
 import search from '../Navbar/search-outline.svg'
 import dropdown from '../Navbar/dropdown.svg'
-import bg from '../Navbar/bg-image.png'
 
-export { cart, logo, search, dropdown, bg }
+export { cart, logo, search, dropdown }
