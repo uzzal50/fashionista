f7093aa23578e022c218f4b87b0a1872afe46ce4
Uzzwal Maharjan <ujolmaharjan94@gmail.com>
2023-06-04 18:25:50 +0545

    added

--- src/components/T-shirt/TshirtItem.jsx
+++ src/components/T-shirt/TshirtItem.jsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
-import bgimage from '../../assets/bg-image.png'
+
 import quickViewIcon from '../../assets/icons/quick-view.svg'
 import { useEffect, useState } from 'react'
 
